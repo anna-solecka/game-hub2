c108d9d21d49ebb804ea370d9489b656722da364
ANNA SOLECKA <ania.solecka@gmail.com>
2024-06-05 09:51:17 +0100

    Buid the color mode switch

--- src/App.tsx
+++ src/App.tsx
@@ -10,14 +10,10 @@ function App() {
         <NavBar />
       </GridItem>
       <Show>
-        <GridItem area="aside" bg="gold">
-          Aside
-        </GridItem>
+        <GridItem area="aside">Aside</GridItem>
       </Show>
 
-      <GridItem area="main" bg="dodgerblue">
-        Main
-      </GridItem>
+      <GridItem area="main">Main</GridItem>
     </Grid>
   );
 }
