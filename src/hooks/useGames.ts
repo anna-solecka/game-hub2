5f967c4fa0d1f6b8c5905c7a16d0f9c79d6d1e1a
ANNA SOLECKA <ania.solecka@gmail.com>
2024-06-10 14:30:00 +0100

    Displaying platform icons

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -3,11 +3,17 @@ import apiClients from "../services/api-clients";
 import { CanceledError } from "axios";
 
 
+export interface Platform {
+    id:number;
+    name:string;
+    slug:string;
+}
 
     export interface Game {
         id: number;
         name: string;
         background_image: string;
+        parent_platforms: {platform : Platform}[];
       }
       
       interface FetchGamesResponse {
