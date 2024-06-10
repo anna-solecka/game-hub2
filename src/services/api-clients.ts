5cdf8669f0193ce9ce4ae8cb45c2333d4f80ceac
ANNA SOLECKA <ania.solecka@gmail.com>
2024-06-10 10:44:31 +0100

    New API key added

--- src/services/api-clients.ts
+++ src/services/api-clients.ts
@@ -3,6 +3,6 @@ import axios from "axios";
 export default axios.create ({
     baseURL: 'https://api.rawg.io/api',
     params: {
-        key: '392f73b6ac234910b30fc9ac0f3bf7e6'
+        key: 'b7cab14f11844969af543b16da5de64b'
     }
 })
